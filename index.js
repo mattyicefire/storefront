7d1d3de30f33dfd0544b0292573ed7945884b251
mattyicefire <81605759+mattyicefire@users.noreply.github.com>
2022-04-15 21:31:22 -0400

    editing works, posting soon

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 
 const Product = require('./models/product');
+const { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } = require('constants');
 
 mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
@@ -15,16 +16,40 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/products', async (req, res) => {
     const products = await Product.find({})
     res.render('products/index' , { products })
 })
 
+app.get('/products/new', (req, res) => {
+    res.render('products/new')
+})
+
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`);
+})
+
 app.get('/products/:id', async (req, res) => {
-    const { id } = req.params.id;
+    const {id} = req.params;
     const product = await Product.findById(id)
+    console.log(product);
     res.render('products/show', { product })
+})
+
+app.get('/products/:id/edit', async (req, res) => {
+    const {id} = req.params;
+    const product = await Product.findById(id)
+    res.render('products/edit', { product })
+})
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+    res.redirect(`/products/${product._id}`);
 })
 
 app.listen(3000, () => {
